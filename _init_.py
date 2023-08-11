9f073c9bfb1c7745b5283173b0a416ecd82e927c
Sonit Patil <141321942+SRodriguezIITH@users.noreply.github.com>
2023-08-11 11:47:50 +0530

    Create _init_.py

---
+++ _init_.py
@@ -0,0 +1,6 @@
+from empver import empverfun
+from manver import manverfun
+from empreg import empregfun
+from empactfun import empactfunr, empactfunw
+from autopdf import pdfn
+from loadf import loadbar
