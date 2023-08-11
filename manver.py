f70535706a4b28dcb3bb01b09d33c4993438dcc2
Sonit Patil <141321942+SRodriguezIITH@users.noreply.github.com>
2023-08-11 11:41:56 +0530

    Create manver.py

---
+++ manver.py
@@ -0,0 +1,9 @@
+#Manager Verification Module
+def manverfun(mmcd):
+ mmc = "man@suncb23"
+Page | 12
+ if mmc == mmcd:
+ res=("Manager Verified!")
+ else:
+ res=("Incorrect Password.....")
+ return res
