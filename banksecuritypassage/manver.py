620deb2d11e4fa2cc059fac2c1c35a364fd10e7f
Sonit Patil <141321942+SRodriguezIITH@users.noreply.github.com>
2023-12-27 15:55:41 +0530

    Update and rename manver.py to banksecuritypassage/manver.py

--- banksecuritypassage/manver.py
+++ banksecuritypassage/manver.py
@@ -1,9 +1,10 @@
 #Manager Verification Module
 def manverfun(mmcd):
  mmc = "man@suncb23"
-Page | 12
+
  if mmc == mmcd:
  res=("Manager Verified!")
  else:
  res=("Incorrect Password.....")
+ 
  return res
